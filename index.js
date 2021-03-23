d7bff25319f610543db370ecbd683c36793fd74d
emilyrosehamm <72118988+emilyrosehamm@users.noreply.github.com>
2021-03-22 21:18:50 -0500

    animation
    created hover and click for nav and modal

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("your index.js file is loaded correctly!")
+$ (document).ready (function () {
+    $("#contactmebutton").click (function () {
+        $(".popup").fadeIn("slow");   
+        });
+        $(".close").click (function () {
+            $(".popup").fadeOut("slow");   
+            });    
+});
